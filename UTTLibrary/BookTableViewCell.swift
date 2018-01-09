687d5120b4ea614bd7e433a97da960f92938f637
Sy Hung NGHIEM <syhungnghiem@Sys-Mac.local>
2018-01-09 06:20:17 -0800

    Initial Commit

---
+++ UTTLibrary/BookTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BookTableViewCell.swift
+//  UTTLibrary
+//
+//  Created by Sy Hung NGHIEM on 26/12/2017.
+//  Copyright © 2017 Sy Hung NGHIEM. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UTTLibrary/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UTTLibrary
+//
+//  Created by Sy Hung NGHIEM on 26/12/2017.
+//  Copyright © 2017 Sy Hung NGHIEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var bookImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
